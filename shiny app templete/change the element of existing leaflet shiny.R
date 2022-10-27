1f835546c83e9aab1cc2eedc943f6226958be9cd
proxima-j <32122209+proxima-j@users.noreply.github.com>
2022-10-27 01:43:15 -0500

    leaflet shinny app templete

---
+++ shiny app templete/change the element of existing leaflet shiny.R
@@ -0,0 +1,91 @@
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+
+#bootstrap just improving the interface of shiny, like the alert or 
+#header or other things(aesthetic elements)
+#follow the html on the boostrap side to writ the function as needed
+ui <- bootstrapPage(
+  #define the style interface( the page)
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("map", width = "100%", height = "100%"),
+  absolutePanel(top = 10, right = 10,
+                sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
+                            value = range(quakes$mag), step = 0.1
+                ),
+                selectInput("colors", "Color Scheme",
+                            rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                ),
+                checkboxInput("legend", "Show legend", TRUE)
+  )
+)
+
+server <- function(input, output, session) {
+  
+  # Reactive expression for the data subsetted to what the user selected
+  filteredData <- reactive({
+    quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+  })
+  
+  # This reactive expression represents the palette function,
+  # which changes as the user makes selections in UI.
+  colorpal <- reactive({
+    colorNumeric(input$colors, quakes$mag)
+  })
+  
+  output$map <- renderLeaflet({
+    # Use leaflet() here, and only include aspects of the map that
+    # won't need to change dynamically (at least, not unless the
+    # entire map is being torn down and recreated).
+    leaflet(quakes) %>% addTiles() %>%
+      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+  })
+  
+  # Incremental changes to the map (in this case, replacing the
+  # circles when a new color is chosen) should be performed in
+  # an observer. Each independent set of things that can change
+  # should be managed in its own observer.
+  observe({
+    pal <- colorpal()
+    
+    leafletProxy("map", data = filteredData()) %>%
+      clearShapes() %>%
+      addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
+                 fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
+      )
+  })
+  
+  # # Use a separate observer to recreate the legend as needed.
+  # observe({
+  #   proxy <- leafletProxy("map", data = quakes)
+  #   
+  #   # Remove any existing legend, and only if the legend is
+  #   # enabled, create a new one.
+  #   proxy %>% clearControls()
+  #   if (input$legend) {
+  #     pal <- colorpal()
+  #     proxy %>% addLegend(position = "bottomright",
+  #                         pal = pal, values = ~mag
+  #     )
+  #   }
+  # })
+  
+  #another way of changing element
+  observe({
+    proxy <- leafletProxy("mymap")
+    
+    # Fit the view to within these bounds (can also use setView)
+    proxy %>% fitBounds(0, 0, 11, 11)
+    
+    # Create circles with layerIds of "A", "B", "C"...
+    proxy %>% addCircles(1:10, 1:10, layerId = LETTERS[1:10])
+    
+    # Remove some of the circles
+    proxy %>% removeShape(c("B", "F"))
+    
+    # Clear all circles (and other shapes)
+    proxy %>% clearShapes()
+  })
+}
+
+shinyApp(ui, server)
---
+++ shiny app templete/leaflet shinny app templete markersdirectly on render.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(leaflet)
+
+
+#creating a shinny app with markers label on map
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+  actionButton("recalc", "New points")
+)
+
+server <- function(input, output, session) {
+
+  #change the size of the map 
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = points())
+  })
+}
+
+shinyApp(ui, server)
