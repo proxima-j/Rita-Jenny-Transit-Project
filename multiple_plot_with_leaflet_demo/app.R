5450b890e1740773cb6c625f13eae5b296612035
proxima-j <32122209+proxima-j@users.noreply.github.com>
2022-11-27 23:05:37 -0600

    create a shiny app demo with line graph and leaflet, but log error happen when publishing

--- multiple_plot_with_leaflet_demo/app.R
+++ multiple_plot_with_leaflet_demo/app.R
@@ -1,8 +1,19 @@
 library(shiny)
 library(shinydashboard)
 library(DT)
+library(shinyWidgets)
+library(rgdal)
+library(leaflet)
+library (raster)
+library(dplyr)
+library(sf)
+library(tidyverse)
+#load data
+map <- readOGR("newmap.shp") %>%
+  st_as_sf()
+ridership<- read_csv("rider_gas_complete.csv")
 # Define UI for application that draws a histogram
-ui <- dashboardPage(
+ui <- dashboardPage( skin="purple",
   dashboardHeader(title = "Gas Price and Ridership"),
   dashboardSidebar(#add sidebar menu
     sidebarMenu(
@@ -11,39 +22,97 @@ ui <- dashboardPage(
     )),
   dashboardBody(#Add tab Item
     tabItems(
-      tabItem("iris",
-              box(plotOutput("correlation_plot"), width = 8),
+      tabItem("Single_Route",
+              box(leafletOutput("routesLeaflet"), width = 8),
               # drop down list(add interactivity):set ID, add lable feature, a vector with possible choices
               box(
-                selectInput(
-                  "features",
-                  "Features:",
-                  c("Sepal.Width", "Petal.Width", "Petal.Length")
-                ), width = 4
-              )),
+                selectInput("pick_routes", "choose a route:", choices = c(unique(map$route))), width = 4
+              ),
+              box(plotOutput("ridership_plot"), width = 6, length= 4),
+              box(plotOutput("gas_price_plot"), width = 6, length= 4)),
       tabItem("cars",
               fluidPage(h1("Cars"),
                         # add data table item
                         dataTableOutput("carstable")
               ))
-      #move this whole plot to the tabItem
-      # #add a box to store the plot output, ans this specific plot is named "correlation plot"
-      # box(plotOutput("correlation_plot"), width= 8),
-      # # drop down list(add interactivity):set ID, add lable feature, a vector with possible choices
-      # box(
-      #   selectInput("features", "Features:",
-      #               c("Sepal.Width","Petal.Width","Petal.Length")), width = 4
-      # )
+     
     ))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  #render the correlation plot with the iris dataset
-  output$correlation_plot <- renderPlot({
-    #make the dropdown list the input into the plot function
-    plot(iris$Sepal.Length,iris[[input$features]],
-         xlab = "Sepal length", ylab = "Feature")
+  filtered <- reactive({
+    filter(map, map$route == input$pick_routes)
+  })
+  filtere_rider <- reactive({
+    ridership %>%
+      filter(Route == input$pick_routes)
+  })
+  filteredClass<- reactive({
+    filter(map, map$RouteClass %in%  c(input$classSelect))
+  })
+  output$ridership_plot <- renderPlot({
+     
+    ggplot(filtere_rider(),aes(x = date))+
+      geom_line(aes(y = riders/coef-1),color = "#3F4345")+ 
+      scale_y_continuous(
+        name = NULL,
+        sec.axis = sec_axis(trans = ~.*coef-1))+
+      labs(title = "Rides of Selected Routes Over Time",
+           subtitle = "From Jan 2014 to Oct 2017")+
+      geom_text(aes(x = 2017.5, y = 2.85), label = "Riders",color = "#3F4345",size = 4)+
+      theme_minimal()+
+      theme(axis.text.y.left =element_blank(),
+            axis.title.y.right = element_text(color = "#3F4345",size = 12),
+            
+            axis.text.y.right = element_text(color = "#3F4345",size = 8),
+            axis.title.x.bottom = element_blank(),
+            plot.title = element_text(color = "#006bb3",size = 14),
+            plot.subtitle = element_text(color = "#006bb3"))
+  })
+  output$gas_price_plot <- renderPlot({
+  
+    ggplot(filtere_rider(),aes(x = date)) +
+      geom_line(aes(y = gas_price), color = "#9F2C2C") +
+      scale_y_continuous(name = NULL,) +
+      scale_x_continuous(name = NULL,) +
+      labs(title = "Gas Price Over Time",
+           subtitle = "From Jan 2014 to Oct 2017") +
+      geom_text(
+        aes(x = 2017, y = 2.85),
+        label = "Gas Price",
+        color = "#9F2C2C",
+        size = 4
+      ) +
+      theme_minimal() +
+      theme(
+        plot.title = element_text(color = "#006bb3", size = 14),
+        plot.subtitle = element_text(color = "#006bb3")
+      )
+  })
+  #create map
+  output$routesLeaflet <- renderLeaflet({
+    #setup map
+    routesLeaflet <-
+      #map%>%
+      leaflet() %>%
+      addProviderTiles("CartoDB.Positron",
+                       options = providerTileOptions(opacity = 0.8)) %>%
+      addPolylines(
+        data = filtered(),
+        color = "navy",
+        weight = 3,
+        smoothFactor = 0.5,
+        opacity = 1.0,
+        highlightOptions = highlightOptions(
+          color = "cyan",
+          weight = 2,
+          bringToFront = TRUE
+        ),
+        popup =  paste("Route: ", map$route, "<br>")
+      )
+    #call map
+    routesLeaflet
   })
   #render the car table, just put in the data name
   output$carstable <- renderDataTable(mtcars)
